da2785c865681b47e54ef5ca2b066ab336eec05b
Nitesh Bagle <niteshbagle43@gmail.com>
2023-01-27 19:08:00 +0530

    wip

---
+++ typescript-project-setup/enums.js
@@ -0,0 +1,8 @@
+var ROLE;
+(function (ROLE) {
+    ROLE[ROLE["ADMIN"] = 0] = "ADMIN";
+    ROLE[ROLE["READ_ONLY"] = 1] = "READ_ONLY";
+    ROLE[ROLE["AUTHOR"] = 2] = "AUTHOR";
+})(ROLE || (ROLE = {}));
+;
+var role = 2;
---
+++ typescript-project-setup/enums.ts
@@ -0,0 +1,13 @@
+enum ROLE {
+    ADMIN,
+    READ_ONLY,
+    AUTHOR
+};
+
+enum ROLE1 {
+    ADMIN = 5,
+    READ_ONLY,
+    AUTHOR
+};
+
+const role: ROLE = 2;
