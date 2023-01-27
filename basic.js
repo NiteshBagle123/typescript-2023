1621bb5cb1e7a64568672ee3d3631ea740045921
Nitesh Bagle <niteshbagle43@gmail.com>
2023-01-27 13:01:28 +0530

    wip

---
+++ basic.js
@@ -0,0 +1,11 @@
+var button = document.getElementById("button");
+var input1 = document.getElementById("num1");
+var input2 = document.getElementById("num2");
+function add(num1, num2) {
+    return num1 + num2;
+}
+if (button) {
+    button.addEventListener("click", function () {
+        console.log(add(+input1.value, +input2.value));
+    });
+}
