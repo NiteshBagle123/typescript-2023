f902a4bef4593091a6f88b13f93c3356dbd632be
Nitesh Bagle <niteshbagle43@gmail.com>
2023-01-27 16:36:33 +0530

    type basic

--- typescript-project-setup/app.js
+++ typescript-project-setup/app.js
@@ -1,1 +1,7 @@
-console.log('Your code goes here123....!!!');
+function add(num1, num2) {
+    return num1 + num2;
+}
+var num1 = 5;
+var num2 = '2.8';
+var response = add(num1, num2);
+console.log('Print sum', response);
--- typescript-project-setup/app.ts
+++ typescript-project-setup/app.ts
@@ -1,1 +1,8 @@
-console.log('Your code goes here123....!!!')
+function add (num1: number, num2: number) {
+    return num1 + num2;
+}
+
+const num1 = 5;
+const num2 = 2.8;
+const response = add(num1, num2);
+console.log('Print sum', response);
