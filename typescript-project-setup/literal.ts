3342a710f6ef0f25b52a669c73e99396c5207536
Nitesh Bagle <niteshbagle43@gmail.com>
2023-01-27 19:31:55 +0530

    literals

---
+++ typescript-project-setup/literal.ts
@@ -0,0 +1,9 @@
+const person: {
+    name: string | number,
+    age: string | number,
+    role: 'ADMIN' | 'USER' | 'USER_ADMIN'
+} = {
+    name: 'Nitesh Bagle',
+    age: 27,
+    role: 'ADMIN'
+}
