d57a2b025e89d96d3fe24ee64a4f94d1b4d443c5
Nitesh Bagle <niteshbagle43@gmail.com>
2023-01-27 18:50:24 +0530

    wip

---
+++ typescript-project-setup/arrays.js
@@ -0,0 +1,9 @@
+var person = {
+    name: 'Nitesh Bagle',
+    age: 27,
+    hobbies: ['Sports', 'trekking', 'reading']
+};
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log('Print hobby', hobby);
+}
---
+++ typescript-project-setup/arrays.ts
@@ -0,0 +1,9 @@
+const person = {
+    name: 'Nitesh Bagle',
+    age: 27,
+    hobbies: ['Sports', 'trekking', 'reading']
+}
+
+for (let hobby of person.hobbies){
+    console.log('Print hobby', hobby);
+}
