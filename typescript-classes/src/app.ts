eabd0b19ceef8df93caee8c5888fbba170e17a6d
Nitesh Bagle <niteshbagle43@gmail.com>
2023-03-13 23:49:25 +0530

    optional parameter

--- typescript-classes/src/app.ts
+++ typescript-classes/src/app.ts
@@ -1,19 +1,36 @@
+// type addFn = (a: number, b: number) => number;
+interface addFn {
+    (a: number, b: number): number;
+}
+
+let add: addFn;
+add = (a, b) => {
+    return a + b;
+}
+
 interface Named {
-    readonly name: string;
+    readonly name?: string;
+    outputName?: string;
 }
 interface Greetable extends Named {
     greet(phrase: string): void;
 }
 
 class Person implements Greetable {
-    name: string;
-    constructor(n: string) {
-        this.name = n;
+    name?: string;
+    constructor(n?: string) {
+        if(n) {
+            this.name = n;
+        }
     }
     greet(phrase: string) {
-        console.log(phrase, this.name);
+        if(this.name){
+            console.log(phrase, this.name);
+        } else {
+            console.log(phrase);
+        }
     }
 }
 let personObj: Greetable;
-personObj = new Person('Nitesh');
+personObj = new Person();
 personObj.greet('Hello Nitesh!');
