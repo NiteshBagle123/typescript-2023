f432f30d1f9b92aa0c4e4c10a3a5fb88dbd1fed6
Nitesh Bagle <niteshbagle43@gmail.com>
2023-03-19 19:42:01 +0530

    generics

---
+++ generics-session-5/src/app.ts
@@ -0,0 +1,26 @@
+// Generic types
+const names: Array<string> = ['Nitesh', 'Shraddha'];
+
+// Promise generic type
+const promise: Promise<string> = new Promise((reject, resolve) => {
+    setTimeout(() => {
+        resolve('After 2 milliseconds');
+    }, 2000);
+    reject('Failed');
+});
+
+promise.then(() => {
+    console.log('Promise resolved');
+}).catch(() => {
+    console.log('Promise rejected');
+})
+
+// custom generic function
+function mergeObjects<T, U>(objA: T, objB: U) {
+    return {
+        ...objA,
+        ...objB
+    }
+};
+const mergedObject = mergeObjects({ name: 'Nitesh'}, { age: 27 });
+console.log(mergedObject);
