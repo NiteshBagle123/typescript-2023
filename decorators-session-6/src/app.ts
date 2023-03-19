4c5e07873fcc0b1425afa97c8648fa0c3075d937
Nitesh Bagle <niteshbagle43@gmail.com>
2023-03-20 00:10:26 +0530

    decorators with templates

--- decorators-session-6/src/app.ts
+++ decorators-session-6/src/app.ts
@@ -1,8 +1,38 @@
-function Logger(constructor: Function) {
-    console.log('Logging...', constructor);
+// first decorator function
+// function Logger(constructor: Function) {
+//     console.log('Logging...', constructor);
+// }
+
+// @Logger
+// class Person {
+//     name = 'Nitesh';
+//     constructor(){
+//         console.log('Creating person object...')
+//     }
+// }
+
+// const person = new Person();
+
+function Logger(logString: string) {
+   return function(constructor: Function) {
+    console.log('constructor', constructor);
+    console.log('logString', logString);
+   }
 }
 
-@Logger
+function withTemplate(template: string, hookId: string) {
+    return function(_: Function) {
+        const hookElement = document.getElementById('app');
+        if(hookElement){
+            hookElement.innerHTML = template;
+        }
+        console.log('template', template);
+        console.log('hookId', hookId);
+    }
+}
+
+// @Logger('LOGGING-STRING')
+@withTemplate('<h1>My Person</h1>', 'app')
 class Person {
     name = 'Nitesh';
     constructor(){
