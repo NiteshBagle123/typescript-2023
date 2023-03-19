3a000d55c9f79fe601acb631b62f0395bd32cbc7
Nitesh Bagle <niteshbagle43@gmail.com>
2023-03-19 19:53:44 +0530

    generic function

--- generics-session-5/src/app.ts
+++ generics-session-5/src/app.ts
@@ -25,3 +25,16 @@ function mergeObjects<T extends object, U extends object>(objA: T, objB: U) {
 
 const mergedObject = mergeObjects({ name: 'Nitesh'}, { age: 27 });
 console.log(mergedObject);
+
+// custom generic function 2
+interface Lengthy {
+    length: number
+}
+function countAndPrint<T extends Lengthy>(ele: T): [T, string] {
+    let description = 'Got no element'
+    if(ele.length){
+        description = `Got element with count: ${ele.length}`;
+    }
+    return [ele, description];
+}
+console.log(countAndPrint(['Sports', 'Cooking']));
