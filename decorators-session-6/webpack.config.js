e5f8037d39b090b9ea883f530e423c90a4357bd0
Nitesh Bagle <niteshbagle43@gmail.com>
2023-03-20 17:33:43 +0530

    webpack configuration

---
+++ decorators-session-6/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+ 
+module.exports = {
+  mode: 'development',
+  entry: './src/app.ts',
+  devServer: {
+    static: [
+      {
+        directory: path.join(__dirname),
+      },
+    ],
+  },
+  devtool: 'inline-source-map',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/dist/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+};
